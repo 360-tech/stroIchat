fa9ee19dfde550b4dd5a0c0704e296cb019a0d94
Nikita S <cybergamer1992@gmail.com>
2025-11-11 11:50:31 +0300

    abort: team.go changes

--- server/channels/api4/team.go
+++ server/channels/api4/team.go
@@ -1571,6 +1571,11 @@ func inviteUsersToTeam(c *Context, w http.ResponseWriter, r *http.Request) {
 
 func inviteGuestsToChannels(c *Context, w http.ResponseWriter, r *http.Request) {
 	graceful := r.URL.Query().Get("graceful") != ""
+	if c.App.Channels().License() == nil {
+		c.Err = model.NewAppError("Api4.InviteGuestsToChannels", "api.team.invite_guests_to_channels.license.error", nil, "", http.StatusNotImplemented)
+		return
+	}
+
 	if !*c.App.Config().GuestAccountsSettings.Enable {
 		c.Err = model.NewAppError("Api4.InviteGuestsToChannels", "api.team.invite_guests_to_channels.disabled.error", nil, "", http.StatusNotImplemented)
 		return
@@ -1590,11 +1595,7 @@ func inviteGuestsToChannels(c *Context, w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	license := c.App.Channels().License()
-	guestEnabled := license == nil
-	if license != nil && license.Features.GuestAccounts != nil && *license.Features.GuestAccounts {
-		guestEnabled = true
-	}
+	guestEnabled := c.App.Channels().License() != nil && *c.App.Channels().License().Features.GuestAccounts
 
 	if !guestEnabled {
 		c.Err = model.NewAppError("Api4.InviteGuestsToChannels", "api.team.invite_guests_to_channels.disabled.error", nil, "", http.StatusForbidden)
