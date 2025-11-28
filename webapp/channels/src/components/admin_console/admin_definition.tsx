2d457e006dde92c8b76cd7b5cf98dc026e3a3d83
Nikita S <cybergamer1992@gmail.com>
2025-11-28 12:08:07 +0300

    update: change default site name from 'Mattermost' to 'Stroichat' in admin console and localization files

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -2146,7 +2146,7 @@ const AdminDefinition: AdminDefinitionType = {
                             key: 'TeamSettings.SiteName',
                             label: defineMessage({id: 'admin.team.siteNameTitle', defaultMessage: 'Site Name:'}),
                             help_text: defineMessage({id: 'admin.team.siteNameDescription', defaultMessage: 'Name of service shown in login screens and UI. When not specified, it defaults to "Mattermost".'}),
-                            placeholder: defineMessage({id: 'admin.team.siteNameExample', defaultMessage: 'E.g.: "Mattermost"'}),
+                            placeholder: defineMessage({id: 'admin.team.siteNameExample', defaultMessage: 'E.g.: "Stroichat"'}),
                             max_length: Constants.MAX_SITENAME_LENGTH,
                             isDisabled: it.not(it.userHasWritePermissionOnResource(RESOURCE_KEYS.SITE.CUSTOMIZATION)),
                         },
